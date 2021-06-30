8bad887296380156df8dd62b095c0cce74c49b5b
Froloket64 <ket.frol2006@gmail.com>
2021-06-30 11:00:48 +0300

    Updated autostart list

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,7 +1,25 @@
-#!/usr/bin/env bash 
+#!/usr/bin/bash
 
-# lxsession &
+## DISCLAIMER: The lower "if" line disables
+## an input device with ID 11 (in my case,
+## the touchpad that breaks Qtile for some reason ;) ).
+##
+## I set the envvar to make it work, so you won't get
+## influenced by this, however, be aware that
+## IDs of devices are different on different PCs,
+## so you can easily disable smth like your mouse
+## using this line, which doesn't seem like fun, does it?
+##
+## PS: See list of devices using `xinput` command
+## with no args/options
+
+if [ $DISABLE_TOUCHPAD = true ]; then
+	xinput --disable 11 &
+fi
+lxappearance &
 picom --experimental-backends &
 nitrogen --restore &
 volumeicon &
 nm-applet &
+timeshift &
+# lxsession &
