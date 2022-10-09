51418c27b2fa781c8ccb00b8486df792b42b5f72
Froloket64 <ket.frol2006@gmail.com>
2022-10-09 21:52:41 +0300

    `fish`: Added checks for external progs; edits

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -1,24 +1,28 @@
 ###  Oh My Fish config  ###
 
-# Path to Oh My Fish install.
+## Path to OMF install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-
-# Load Oh My Fish configuration.
+## Load OMF
 source $OMF_PATH/init.fish
 
-# Load aliases
-source $HOME/.config/fish/conf.d/aliases.fish
+## Env vars
+export LC_ALL=en_US.utf8
 
-# Env vars
-export LC_ALL=en_US.utf8  # Breaks all glyphs
-export LS_COLORS=(vivid generate gruvbox-dark-soft)
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+# Manpager
+if command -vq bat
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+end
 
-# Other vars/overrides
-set fish_greeting  # Disbale the startup intro message
+# LSColors
+if command -vq vivid
+    export LS_COLORS=(vivid generate gruvbox-dark-soft) # Needs `vivid`
+end
 
-# Exec on startup
-neofetch  # how d'ya live without it ;)
+## Other vars/overrides
+set fish_greeting # Disbale the startup intro message
+
+## Exec on startup
+neofetch # how d'ya live without it ;)
