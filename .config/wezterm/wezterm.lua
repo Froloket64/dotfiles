865a104d719a8a5f845bdc493927f2106737001f
Froloket64 <ket.frol2006@gmail.com>
2022-10-09 22:02:38 +0300

    `wezterm`: Removed window padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -21,6 +21,13 @@ return {
     use_fancy_tab_bar = false,
     font_size = 10,
     window_background_opacity = 0.75,
+    -- max_fps = 120,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 
     -- Key binds
     keys = {
