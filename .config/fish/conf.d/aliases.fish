15ea60467393bd07c9cb49496899ef14d595426f
Froloket64 <ket.frol2006@gmail.com>
2022-11-12 21:27:18 +0300

    `install.sh`: Added checks for installed commands

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,14 +1,20 @@
 ## Aliases
 # --Commands-- ###
-alias ls="lsd"  # ;)
-alias ll="lsd -l"
+if command -v lsd >/dev/null
+    alias ls="lsd"
+    alias clt="clear; lsd --tree"
+end
+
+alias ll="ls -l"
 alias cl="clear; ll"
-alias clt="clear; lsd --tree"
 alias myip="curl zx2c4.com/ip"
 alias new="$TERM --working-directory=\$PWD -e fish & test"
 alias logout="pkill -KILL -u $USER"
 
 # --Scripts-- ###
 alias alarm="notify-send --icon /usr/share/icons/Papirus/16x16/actions/dialog-error.svg 'Time is UP!'"
-alias pwd=ptpwd
-alias cp=ptcp
+
+if command -v ptsh >/dev/null
+    alias pwd=ptpwd
+    alias cp=ptcp
+end
