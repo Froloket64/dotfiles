610ef1715a80e6d26587142f7da5230b8db31c64
Froloket64 <ket.frol2006@gmail.com>
2022-11-19 16:54:42 +0300

    `fish`: Added more checks; aliases command check

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,19 +1,29 @@
 ## Aliases
-if command -v lsd >/dev/null
+alias check="command -v \$1 >/dev/null"
+
 # Shell usage
+if check lsd
     alias ls="lsd"
     alias clt="clear; lsd --tree"
 end
 
 alias ll="ls -l"
 alias cl="clear; ll"
-alias myip="curl zx2c4.com/ip"
 alias new="$TERM --working-directory=\$PWD -e fish & test"
-alias logout="pkill -KILL -u $USER"
 
-alias alarm="notify-send --icon /usr/share/icons/Papirus/16x16/actions/dialog-error.svg 'Time is UP!'"
-
-if command -v ptsh >/dev/null
+if check ptsh
     alias pwd=ptpwd
     alias cp=ptcp
+end
+
+# Other
+alias logout="pkill -KILL -u $USER"
+alias myip="curl zx2c4.com/ip"
+
+if check notify-send
+    alias alarm="notify-send --icon /usr/share/icons/Papirus/16x16/actions/dialog-error.svg 'Time is UP!'"
+end
+
+if check flatpak
+   alias flatrun="flatpak run (flatpak list --columns=application | fzf --reverse --height=~10 | awk '{ print \$2 }')"
 end
