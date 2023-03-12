4a9298d5661b096a9aa73a9f920712c7c6726457
Froloket64 <ket.frol2006@gmail.com>
2023-03-12 19:20:58 +0300

    `fish`: Add function to use `broot`

--- template/.config/fish/conf.d/functions.fish
+++ template/.config/fish/conf.d/functions.fish
@@ -4,3 +4,17 @@ function recomp_dots
     ./install.sh -I
     cd -
 end
+
+# Execute broot and command it returns
+function br --wraps=broot
+    set -l cmd_file (mktemp)
+    if broot --outcmd $cmd_file $argv
+        read --local --null cmd < $cmd_file
+        rm -f $cmd_file
+        eval $cmd
+    else
+        set -l code $status
+        rm -f $cmd_file
+        return $code
+    end
+end
