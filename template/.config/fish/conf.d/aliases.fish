8a6a08d7da3eef52831ae5efe0f8aae9f67373ca
Froloket64 <ket.frol2006@gmail.com>
2023-03-12 17:11:32 +0300

    `fish`: Removed `=` in alias declarations

--- template/.config/fish/conf.d/aliases.fish
+++ template/.config/fish/conf.d/aliases.fish
@@ -4,29 +4,29 @@ set ls_exec ls
 if command -vq lsd
     set ls_exec lsd
 
-    alias lst="lsd --tree"
-    alias clt="clear; lsd --tree"
+    alias lst "lsd --tree"
+    alias clt "clear; lsd --tree"
 end
 
 {%- if features.lsSingleLine %}
 # One file on each line
-alias ls="$ls_exec -1"
+alias ls "$ls_exec -1"
 {%- endif %}
 
-alias cl="clear; ls"
-alias new="$TERM --working-directory=\$PWD -e fish & test"
+alias cl "clear; ls"
+alias new "$TERM --working-directory=\$PWD -e fish & test"
 
 if command -vq ptsh
-    alias pwd=ptpwd
-    alias cp=ptcp
+    alias pwd ptpwd
+    alias cp ptcp
 end
 
 # Daily routine
-alias ec="emacsclient -nw"
-alias lg="lazygit"
+alias ec "emacsclient -nw"
+alias lg "lazygit"
 
 # Other
-alias logout="pkill -KILL -u $USER"
-alias myip="curl zx2c4.com/ip"
-alias alarm="notify-send --icon /usr/share/icons/{{ gtk.iconTheme }}/16x16/actions/dialog-error.svg 'Time is UP!'"
-alias flatrun="flatpak run (flatpak list --columns=application | fzf --reverse --height=~10 | awk '{ print \$2 }')"
+alias logout "pkill -KILL -u $USER"
+alias myip "curl zx2c4.com/ip"
+alias alarm "notify-send --icon /usr/share/icons/{{ gtk.iconTheme }}/16x16/actions/dialog-error.svg 'Time is UP!'"
+alias flatrun "flatpak run (flatpak list --columns=application | fzf --reverse --height=~10 | awk '{ print \$2 }')"
