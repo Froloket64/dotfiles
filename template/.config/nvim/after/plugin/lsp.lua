91ea241889aba9392683f3055ef13eedc06dcbbc
Froloket64 <ket.frol2006@gmail.com>
2023-03-24 14:59:25 +0300

    `nvim`: Fix some LSP config not working; Update it

--- template/.config/nvim/after/plugin/lsp.lua
+++ template/.config/nvim/after/plugin/lsp.lua
@@ -5,13 +5,15 @@ local lsp = require("lsp-zero").preset {
     suggest_lsp_servers = false,
 }
 
-vim.diagnostic.config {
-    virtual_text = true,
-}
-
 lsp.ensure_installed {
     "rust_analyzer",
     "lua_ls"
 }
 
 lsp.setup()
+
+vim.diagnostic.config {
+    virtual_text = true,
+    signs = false,
+    update_in_insert = true,
+}
