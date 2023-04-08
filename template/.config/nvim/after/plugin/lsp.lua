64d6dc873f9a9c5b9a77ee1370b357793d1dc3d0
Froloket64 <ket.frol2006@gmail.com>
2023-04-08 16:29:58 +0300

    `nvim`: Add LSP on_attach remaps

--- template/.config/nvim/after/plugin/lsp.lua
+++ template/.config/nvim/after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 local lsp = require("lsp-zero").preset {
     name = "recommended",
-    set_lsp_keymaps = true,
+    set_lsp_keymaps = false,
     manage_nvim_cmp = true,
     suggest_lsp_servers = false,
 }
@@ -10,6 +10,16 @@ lsp.ensure_installed {
     "lua_ls",
     "marksman",
 }
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "<leader>ci", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>ct", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>cs", vim.lsp.buf.signature_help, opts)
+end)
 
 lsp.setup()
 
