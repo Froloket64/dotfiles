ec7a4f6a1bf4cdc9b5c520e2f595272eb63c6aec
Froloket64 <ket.frol2006@gmail.com>
2023-08-29 10:14:57 +0300

    `nvim`: Change Cmp window style to rounded borders

--- template/.config/nvim/after/plugin/cmp.lua
+++ template/.config/nvim/after/plugin/cmp.lua
@@ -6,6 +6,10 @@ cmp.setup {
             require("luasnip").lsp_expand(args.body)
         end,
     },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     sources = cmp.config.sources {
         { name = "nvim_lsp" },
         { name = "luasnip" },
