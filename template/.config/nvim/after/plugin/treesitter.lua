d67eb79c571bf993d55e0a35f5dd8ae645a5e168
Froloket64 <ket.frol2006@gmail.com>
2023-08-29 10:21:54 +0300

    `nvim`: Edit Treesitter config

--- template/.config/nvim/after/plugin/treesitter.lua
+++ template/.config/nvim/after/plugin/treesitter.lua
@@ -1,39 +1,41 @@
 require("nvim-treesitter.configs").setup {
-  -- Parsers to be installed 
-  ensure_installed = {
-      "rust",
-      "javascript",
-      "python",
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "markdown",
-  },
+    -- Parsers to be installed 
+    ensure_installed = {
+        "rust",
+        "javascript",
+        "python",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+    },
 
-  -- Install parsers synchronously
-  sync_install = false,
+    -- Install parsers synchronously
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = {},
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = {},
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- List of parsers to disable
-    disable = {},
+        -- List of parsers to disable
+        disable = {},
 
-    -- Function to disable treesitter highlight for large files
-    -- disable = function(lang, buf)
-    --     local max_filesize = 100 * 1024 -- 100 KB
-    --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    --     if ok and stats and stats.size > max_filesize then
-    --         return true
-    --     end
-    -- end,
-  },
+        -- Disable treesitter highlight for large files
+        -- disable = function(lang, buf)
+            --     local max_filesize = 100 * 1024 -- 100 KB
+            --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            --     if ok and stats and stats.size > max_filesize then
+            --         return true
+            --     end
+            -- end,
+
+        additional_vim_regex_highlighting = false
+    },
 }
