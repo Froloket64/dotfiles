e6baac946e7e6b0f9cc7b7c4779b0f520cb9a9ea
Froloket64 <ket.frol2006@gmail.com>
2023-08-29 09:13:46 +0300

    `nvim`: Add Hop maps and edit config

--- template/.config/nvim/after/plugin/hop.lua
+++ template/.config/nvim/after/plugin/hop.lua
@@ -1,6 +1,33 @@
-require("hop").setup {
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
+
+hop.setup {
     keys = "etovxqpdygfblzhckisuran",
 }
 
-vim.keymap.set("n", "gw", ":HopWord<cr>")
-vim.keymap.set("n", "gz", ":HopChar1<cr>")
+vim.keymap.set("n", "f", function()
+    hop.hint_char1 {
+        direction = directions.AFTER_CURSOR,
+        current_line_only = true,
+    }
+end)
+vim.keymap.set("n", "F", function()
+    hop.hint_char1 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
+    }
+end)
+vim.keymap.set("n", "t", function()
+    hop.hint_char1 {
+        direction = directions.AFTER_CURSOR,
+        current_line_only = true,
+        hint_offset = -1,
+    }
+end)
+vim.keymap.set("n", "T", function()
+    hop.hint_char1 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
+        hint_offset = 1,
+    }
+end)
