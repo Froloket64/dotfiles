e39e8770342b2fcf1534828f6b2ac9b49de057e8
Froloket64 <ket.frol2006@gmail.com>
2023-08-29 10:14:15 +0300

    `nvim`: Add Ultisnips config; Add extra Cmp maps

--- template/.config/nvim/after/plugin/cmp.lua
+++ template/.config/nvim/after/plugin/cmp.lua
@@ -1,32 +1,85 @@
--- local luasnip = require("luasnip")
 local cmp = require("cmp")
 
-cmp.setup({
-  mapping = {
-    ["<C-J>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }
-    ),
-    ["<C-K>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }
-    ),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.confirm()
-        else
-            fallback()
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    sources = cmp.config.sources {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "ultisnips" },
+        { name = "path" },
+    },
+    formatting = {
+        fields = { "menu", "abbr", "kind" },
+        format = function(entry, item)
+            local menu_icons = {
+                nvim_lsp = "$",
+                luasnip = "λ",
+            }
+
+            item.menu = menu_icons[entry.source.name]
+
+            return item
         end
-    end, { "i", "s" }
-    ),
-    ["<Enter>"] = nil,
-  },
-})
+    },
+    mapping = {
+        -- Select next completion
+        ["<C-j>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }
+        ),
+        -- Select previous completion
+        ["<C-k>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }
+        ),
+        -- Scroll text in the documentation window up
+        ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible then
+                cmp.mapping.scroll_docs(4)
+            else
+                fallback()
+            end
+        end),
+        -- Scroll text in the documentation window down
+        ["<C-p>"] = cmp.mapping(function(fallback)
+            if cmp.visible then
+                cmp.mapping.scroll_docs(-4)
+            else
+                fallback()
+            end
+        end),
+        -- Confirm if visible, otherwise fall back
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.confirm()
+            else
+                fallback()
+            end
+        end, { "i", "s" }
+        ),
+        -- Override <Enter>
+        ["<CR>"] = cmp.mapping(function(fallback)
+            fallback()
+        end),
+        -- Abort the current completion
+        ["<Esc>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.abort()
+            else
+                fallback()
+            end
+        end),
+    },
+}
