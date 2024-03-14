417c9b2d49c868d7cf20572eda04ca6b03730d4c
Froloket64 <ket.frol2006@gmail.com>
2024-03-14 16:01:15 +0300

    `fish`: Add fzf exports

--- template/.config/fish/conf.d/omf.fish
+++ template/.config/fish/conf.d/omf.fish
@@ -15,6 +15,7 @@ set -gx SHELL /bin/fish
 if command -vq bat
     set -gx BAT_THEME "{{ themes.bat }}"
     set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color always {}'"
 end
 
 # LSColors
@@ -27,6 +28,8 @@ set fish_greeting # Disable the startup intro message
 set -gx EDITOR {{ "emacsclient -nw" if general.editor == "emacs" else general.editor }}
 set -gx COLORTERM truecolor
 set -gx MAKEFLAFS "-j$(nproc)"
+set -gx FZF_DEFAULT_COMMAND "fd -H"
+set -gx FZF_CTRL_T_COMMAND "fd -H"
 
 # Cursor shape
 set fish_cursor_default block
